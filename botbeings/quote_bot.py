927e94c22723c2bae0314004a06ea7e43a33978b
Guilhem Allaman <dev@guilhemallaman.net>
2023-09-27 09:31:15 +0200

    Add wikiquotes action to quotebot

--- botbeings/quote_bot.py
+++ botbeings/quote_bot.py
@@ -4,21 +4,28 @@ import requests
 from requests import Response
 
 from botbeings import SuperBotBeing
+from botbeings.pataphysics_bot import random_wikiquote
 
 QUOTABLE_API_BASE_URL = "https://api.quotable.io"
 
 
 class QuoteBotBeing(SuperBotBeing):
     def run(self, action: str = "default") -> None:
-        if action == "default":
-            quote, author = self.fetch_random_quote()
+        if action == "quotable":
+            quote, author = self.fetch_random_quotable()
             content = f'"{quote}"\n\n- {author} -'
             if not self.dryrun:
                 self.mastodon.status_post(content)
             self.logger.info(f'Tooted: "{quote}" -{author}- (length: {len(content)})')
+        elif action == "wikiquote":
+            quote, title = random_wikiquote(lang="en")
+            content = f'"{quote}"\n\n- {title} -'
+            if not self.dryrun:
+                self.mastodon.status_post(content)
+            self.logger.info(f'Tooted: "{quote}" -{title}- (length: {len(content)})')
 
     @staticmethod
-    def fetch_random_quote() -> Tuple[str, str]:
+    def fetch_random_quotable() -> Tuple[str, str]:
         # fetches a quote through quotable API
         r: Response = requests.get(f"{QUOTABLE_API_BASE_URL}/random")
         assert r.status_code == 200
